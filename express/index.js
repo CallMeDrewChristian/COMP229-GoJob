739cb9f071e49dbd39371dcf0bb57ad8b69bfb52
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2024-11-23 16:16:34 -0500

    Added Mongoose + Mongoose Code

--- express/index.js
+++ express/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
+const URL = "";
+
+app.use(express.json());
+
+mongoose.connect(URL)
+.then(() => {
+    console.log("Successfully logged into the database!");
+    app.listen(4000, ()=> {
+        console.log(`App is listening to 4000`);
+    })
+    
+})
 
 app.get("/", (req, res) => {
     res.send("Hello World");
 })
 
-app.listen(4000, ()=> {
-    console.log(`App is listening to 4000`);
+app.post("/postjob", (req, res) => {
+
 })
+
