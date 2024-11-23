028ebae45b65bb13c4d277445fda0cc73a6c375b
Drake374 <drake.kirkos@gmail.com>
2024-11-23 16:29:08 -0500

    Update index.js

--- express/index.js
+++ express/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose')
 const app = express();
+const URL = `mongodb+srv://drakekirkos:1234@webdev.snrip.mongodb.net/`;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+app.use(express.json());
+
+mongoose.connect(URL)
+.then(() => {
+    console.log("Successfully logged into the database!");
+    app.listen(4000, ()=> {
+        console.log(`App is listening to 4000`);
+    })
+    
 })
 
+app.get("/", (req, res) => {
+    res.send("Hello World");
+})
+
+app.post("/postjob", (req, res) => {
 app.listen(4000, ()=> {
     console.log(`App is listening to 4000`);
 
