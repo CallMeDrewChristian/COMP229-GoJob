9d27645d57f00acb71ab809f72af91bd33bee545
Drake374 <drake.kirkos@gmail.com>
2024-11-24 15:08:25 -0500

    Fixed up some of the code, still got problems

--- express/index.js
+++ express/index.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const mongoose = require('mongoose')
 const app = express();
-const URL = `mongodb+srv://drakekirkos:1234@webdev.snrip.mongodb.net/`;
+const URL = `mongodb+srv://drakekirkos:1234@webdev.snrip.mongodb.net/users?retryWrites=true&w=majority&appName=WebDev`;
+const authRoute = require('../routes/authRoutes');
+require('dotenv').config()
+const { requireAuth } = require("../utils/utils");
 
 
 
 app.use(express.json());
+app.use(express.static('public'))
+app.use(cookieParser())
+
+app.set('view engine', 'ejs')
 
 mongoose.connect(URL)
-.then(() => {
-    console.log("Successfully logged into the database!");
-    app.listen(4000, ()=> {
-        console.log(`App is listening to 4000`);
-    })
-    
-})
+    .then(() => {
+        console.log("Successfully logged into the database!");
+        app.listen(4000, () => {
+            console.log(`App is listening to 4000`);
+        })
 
-app.post("/", (req,res) => {
+    })
+    .catch((err) =>{
+        console.log("failed to connect")
+    })
+
+app.post("/", (req, res) => {
     res.send("Bye!");
 })
 app.get("/", (req, res) => {
@@ -24,9 +34,16 @@ app.get("/", (req, res) => {
 })
 
 
-app.post("/postjob", (req, res) => {})
-app.listen(4000, ()=> {
+app.post("/postjob", (req, res) => { })
+app.listen(4000, () => {
     console.log(`App is listening to 4000`);
 
-    
+
 })
+
+app.get('/', requireAuth, (req, res) => res.render('home'))
+app.get('/', requireAuth, (req, res) => res.render('signup'))
+app.get('/', requireAuth, (req, res) => res.render('login'))
+
+
+app.use(authRoute)
