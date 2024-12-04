6030f1730677babb28d2e6ce22773120d30d5dc1
CallMeDrewChristian <72636893+CallMeDrewChristian@users.noreply.github.com>
2024-12-03 22:14:44 -0500

    Added Post

---
+++ Client/src/Post.jsx
@@ -0,0 +1,17 @@
+import logo from './assets/images/logo.jpg';
+import { Link } from 'react-router-dom';
+
+
+
+
+
+function Post() {
+    return(
+        <>
+    
+hello world
+        </>        
+    )
+
+}
+export default Post
